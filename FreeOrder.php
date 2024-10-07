712f3902ad12065fac892bbbb00e770b9b2fdfdb
Franck Allimant <fallimant@openstudio.fr>
2024-10-07 23:20:57 +0200

    Checking order total is 0.0 before payment

--- FreeOrder.php
+++ FreeOrder.php
@@ -14,22 +14,42 @@ namespace FreeOrder;
 
 use Thelia\Core\Event\Order\OrderEvent;
 use Thelia\Core\Event\TheliaEvents;
+use Thelia\Core\Translation\Translator;
+use Thelia\Exception\TheliaProcessException;
 use Thelia\Model\Order;
 use Thelia\Model\OrderStatusQuery;
 use Thelia\Module\AbstractPaymentModule;
 
 class FreeOrder extends AbstractPaymentModule
 {
+    public const MESSAGE_DOMAIN = "freeorder";
+
     /**
      * @return bool
      */
     public function isValidPayment()
     {
-        return round($this->getCurrentOrderTotalAmount(), 4) == 0;
+        return round($this->getCurrentOrderTotalAmount(), 2) === 0.0;
     }
 
+    /**
+     * @param Order $order
+     * @return void
+     * @throws \Propel\Runtime\Exception\PropelException
+     */
     public function pay(Order $order): void
     {
+        // Order total should be zero
+        if ($order->getTotalAmount() > 0.0) {
+            throw new TheliaProcessException(
+                Translator::getInstance()?->trans(
+                    "This payment type is not valid for this order. Please select another payment type.",
+                    [],
+                    self::MESSAGE_DOMAIN
+                )
+            );
+        }
+
         $event = new OrderEvent($order);
         $event->setStatus(OrderStatusQuery::getPaidStatus()->getId());
         $this->getDispatcher()->dispatch($event, TheliaEvents::ORDER_UPDATE_STATUS);
---
+++ I18n/fr_FR.php
@@ -0,0 +1,5 @@
+<?php
+
+return array(
+    'This payment type is not valid for this order. Please select another payment type.' => 'Ce type de paiement n\'est pas valide pour cette commande. Merci de choisir un autre moyen de paiement.',
+);
