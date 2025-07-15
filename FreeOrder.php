3fa870ec9dcda9b93581c27216cd9822d4cb0508
Alexandre <anoziere@openstudio.fr>
2025-07-15 11:14:36 +0200

    php strict compliance

---
+++ FreeOrder.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the Thelia package.
+ * http://www.thelia.net
+ *
+ * (c) OpenStudio <info@thelia.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FreeOrder;
+
+use Symfony\Component\HttpFoundation\Response;
+use Thelia\Core\Event\Order\OrderEvent;
+use Thelia\Core\Event\TheliaEvents;
+use Thelia\Model\Order;
+use Thelia\Model\OrderStatusQuery;
+use Thelia\Module\AbstractPaymentModule;
+
+class FreeOrder extends AbstractPaymentModule
+{
+    /**
+     * @return bool
+     */
+    public function isValidPayment(): bool
+    {
+        return round($this->getCurrentOrderTotalAmount(), 4) == 0;
+    }
+
+    public function pay(Order $order): Response
+    {
+        $event = new OrderEvent($order);
+        $event->setStatus(OrderStatusQuery::getPaidStatus()->getId());
+        $this->getDispatcher()->dispatch($event, TheliaEvents::ORDER_UPDATE_STATUS);
+
+        return new Response('');
+    }
+
+    /**
+     * @return bool
+     */
+    public function manageStockOnCreation(): bool
+    {
+        return false;
+    }
+}
